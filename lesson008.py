3a2ae9a6f0cbc2ce8ae7276d79a67572a8f60243
Mark Peters <mapeters101@earthlink.net>
2025-01-03 19:40:47 -0500

    Work on lesson008 to 5:15.

---
+++ lesson008.py
@@ -0,0 +1,23 @@
+
+print("John")
+print('John')
+
+"John"
+
+name = "John"
+name = 'John'
+
+
+long_string = '''
+line 1
+line 2
+line 3
+'''
+
+print(long_string)
+
+print(name[0])
+print(name[3])
+print(name[2])
+print(name[0:2])
+print(long_string[0:10])
