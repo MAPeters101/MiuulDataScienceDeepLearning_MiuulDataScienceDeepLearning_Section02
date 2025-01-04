a8e20d2f8e3d84068ca4d7f2f49ee495a0ad3357
Mark Peters <mapeters101@earthlink.net>
2025-01-03 19:02:01 -0500

    Work on lesson006 to 6:0.

---
+++ lesson006.py
@@ -0,0 +1,22 @@
+x = 46
+print(type(x))
+
+x = 10.3
+print(type(x))
+
+x = 2j + 1
+print(type(x))
+
+x = "Hello ai era"
+print(type(x))
+
+print(True)
+print(False)
+print(type(True))
+print(5 == 4)
+print(3 == 2)
+print(1 == 1)
+print(type(3 == 2))
+
+
+
